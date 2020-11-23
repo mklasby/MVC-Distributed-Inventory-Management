cfbd3b398428d45c2251491b346c72628dbaaf1d
Tong Xu <jennytong.xu@gamil.com>
2020-11-23 16:42:52 -0700

    Load database and implement DBController for 607-608 project

--- InventoryApp/src/DBController/DBController.java
+++ InventoryApp/src/DBController/DBController.java
@@ -1,28 +1,27 @@
 package DBController;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public abstract class DBController implements DBCredentials {
 	
 	// Attributes
 	protected Connection conn;//Object of type connection from the JDBC class that deals with connecting to 
 	//the database 
-	protected Statement stmt; //object of type statement from JDBC class that enables the creation "Query 
+	protected PreparedStatement stmt; //object of type statement from JDBC class that enables the creation "Query 
 	//statements"
 	protected ResultSet rs;//object of type ResultSet from the JDBC class that stores the result of the query
 
-	public void initializeConnection() {
+	public DBController() {
 		try {
-            //Register JDBC driver
-			Driver driver = new com.mysql.cj.jdbc.Driver();
-			DriverManager.registerDriver(driver);
             //Open a connection
 			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+			String query = "USE toolshop;";
+			stmt = conn.prepareStatement(query);
+			stmt.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println("Problem");
 			e.printStackTrace();
